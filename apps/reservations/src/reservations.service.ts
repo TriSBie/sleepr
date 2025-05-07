64e36a450e957c46d65ef2c6ed0817df1a57056b
KrissTiNa207 <99896326+TriSBie@users.noreply.github.com>
2025-05-07 23:24:41 +0700

    chore: modified configuration with standard mode

--- apps/reservations/src/reservations.service.ts
+++ apps/reservations/src/reservations.service.ts
@@ -4,23 +4,35 @@ import { ClientProxy } from "@nestjs/microservices";
 import { CreateReservationDto } from "./dto/create-reservation.dto";
 import { UpdateReservationDto } from "./dto/update-reservation.dto";
 import { ReservationsRepository } from "./reservations.repository";
+import { map } from "rxjs";
 
 @Injectable()
 export class ReservationsService {
   constructor(
     private readonly reservationRepository: ReservationsRepository,
-    @Inject(PAYMENTS_SERVICE) private readonly paymentsService: ClientProxy,
+    @Inject(PAYMENTS_SERVICE) private readonly paymentsService: ClientProxy, // as ClientProxy return a
   ) {}
 
   async create(
     createReservationDto: CreateReservationDto,
     { email, _id: userId }: UserDto,
   ) {
-    return this.reservationRepository.create({
-      ...createReservationDto,
-      timestamp: new Date(),
-      userId,
-    });
+    // Call the payments service to create a charge
+    return this.paymentsService
+      .send("create_charge", {
+        ...createReservationDto.charge,
+        email,
+      })
+      .pipe(
+        map((res) => {
+          return this.reservationRepository.create({
+            ...createReservationDto,
+            timestamp: new Date(),
+            invoiceId: res.id,
+            userId,
+          });
+        }),
+      );
   }
 
   async findAll() {
